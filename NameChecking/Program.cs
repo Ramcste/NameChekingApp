f9e4114bfd4b1769001b31ee3ece5d2b05ac0cfb
ramcste <lucky.guyram@gmail.com>
2015-06-09 23:49:08 +0600

    Initila commit for Name Regular Expression with dotted Like R.R. Ram or Ramesh R.Ram

---
+++ NameChecking/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NameChecking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string pattern = @"^(\w+)\s*$";
+
+            //string pattern = @"^\d{11}$";
+
+            string pattern = @"^((([A-Z].)*)+[A-Z][a-z]+||([A-Z][a-z]+\s[A-Z].[A-Z][a-z]+))$";
+
+            string input = Console.ReadLine();
+
+            bool isValid = input != null && Regex.IsMatch(input, pattern);
+
+            if (isValid)
+            {
+                Console.WriteLine("Accepted: {0}", input);
+                Console.ReadLine();
+
+            }
+
+            else
+            {
+                Console.WriteLine("invalid");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
